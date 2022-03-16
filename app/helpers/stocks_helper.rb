2565bbb030aea66feeb4845988cd6287823a9b1d
Chris Morris <cmorris@Chriss-iMac-2.local>
2022-03-16 15:20:48 +0000

    Adds Income Statement table

--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -2,4 +2,8 @@ module StocksHelper
   def sort_link(column:, label:)
     link_to(label, list_stocks_path(column: column))
   end
+
+  def currency(num)
+    number_to_currency(num, precision: 0)
+  end
 end
--- app/javascript/controllers/financials_controller.js
+++ app/javascript/controllers/financials_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["quarter", "annual", "quarterSheet", "annualSheet"]
+  static targets = ["quarter", "annual", "quarterIncomeStatement", "annualIncomeStatement", "quarterBalanceSheet", "annualBalanceSheet", "quarterCashFlow", "annualCashFlow"]
 
   static classes = ["unpressed", "pressed", "hide"]
 
@@ -12,28 +12,28 @@ export default class extends Controller {
   quarter(event) {
     event.preventDefault()
 
-    this.quarterSheetTarget.classList.remove(this.hideClass)
-    this.annualSheetTarget.classList.add(this.hideClass)
+    this.quarterBalanceSheetTarget.classList.remove(this.hideClass)
+    this.annualBalanceSheetTarget.classList.add(this.hideClass)
     if (!this.quarterTarget.classList.contains(this.pressedClass)) {
       this.annualTarget.classList.remove(this.pressedClass)
       this.annualTarget.classList.add(this.unpressedClass)
       this.quarterTarget.classList.add(this.pressedClass)
       this.greetingTarget.classList.add(this.pressedClass)
-      this.annualSheetTarget.classList.add(this.hideClass)
+      this.annualBalanceSheetTarget.classList.add(this.hideClass)
     }
   }
 
   annual(event) {
     event.preventDefault()
 
-    this.annualSheetTarget.classList.remove(this.hideClass)
-    this.quarterSheetTarget.classList.add(this.hideClass)
+    this.annualBalanceSheetTarget.classList.remove(this.hideClass)
+    this.quarterBalanceSheetTarget.classList.add(this.hideClass)
     if (!this.annualTarget.classList.contains(this.pressedClass)) {
       this.quarterTarget.classList.remove(this.pressedClass)
       this.quarterTarget.classList.add(this.unpressedClass)
       this.annualTarget.classList.add(this.pressedClass)
       this.greetingTarget.classList.remove(this.pressedClass)
-      this.quarterSheetTarget.classList.add(this.hideClass)
+      this.quarterBalanceSheetTarget.classList.add(this.hideClass)
     }
   }
 }
