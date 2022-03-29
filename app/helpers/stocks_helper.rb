dcb8f989d3eaee0f864629b37a9b0f91123971e8
Chris Morris <cmorris@Chriss-iMac-2.local>
2022-03-29 10:32:18 +0100

    Adds median to analyst ratings and breadcrumb partial

--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -10,6 +10,32 @@ module StocksHelper
     number_to_currency(num, precision: 0)
   end
 
+  def median_analyst_rating(ratings)
+    ratings_array = []
+    ratings["StrongSell"].times do
+      ratings_array.push(1)
+    end
+
+    ratings["Sell"].times do
+      ratings_array.push(2)
+    end
+
+    ratings["Hold"].times do
+      ratings_array.push(3)
+    end
+
+    ratings["Buy"].times do
+      ratings_array.push(4)
+    end
+
+    ratings["StrongBuy"].times do
+      ratings_array.push(5)
+    end
+
+    median_index = (ratings_array.length / 2).round()
+    return ratings_array[median_index]
+  end
+
   def z_score(stock)
 
     # Setup
