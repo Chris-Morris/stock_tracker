9402ea3a620b42c7510b2ecfe4404af32dbc9486
Chris Morris <cmorris@Chriss-iMac-2.local>
2022-04-08 11:01:34 +0100

    Added loading spinner to stocks index page

--- app/javascript/controllers/stocks_controller.js
+++ app/javascript/controllers/stocks_controller.js
@@ -1,12 +1,25 @@
 import { Controller } from '@hotwired/stimulus';
+import { get } from '@rails/request.js';
 
 export default class extends Controller {
+  static values = { url: String }
 
-  static targets = ["loadingSpinner", "stocksTable"]
+  async runScan() {
+    const loader = document.getElementById('loader');
+    loader.style.display = 'block';
 
-  static classes = ["show", "hide"]
-
-  connect() {
-
-  }
-}
+    const stocksContainer = document.getElementById('stocksContainer');
+    stocksContainer.style.display = 'none';
+    const scanSelect = document.getElementById('scan')
+    const scanId = scanSelect.value;
+    const response = await get(this.urlValue + `?scan=${scanId}`, { responseKind: "html" });
+    if (response.ok) {
+      scanSelect.value = scanId;
+      loader.style.display = 'none';
+      stocksContainer.style.display = 'block';
+      console.log("HTML fetched ok", scanId);
+    } else {
+      console.log("HTML not fetched ok");
+    };
+  };
+};
